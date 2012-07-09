aeed2ce578c480fed0c49bd753ac821aef7b48a7
Peter Johnson <insomnia@rcpt.at>
2012-07-09 20:14:34 +0100

    Refactored config structure & logging

---
+++ src/Visor/Cli/Command/Shutdown.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Visor\Cli\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\ArrayInput;
+
+use \Supervisord\Config;
+use \Supervisord\Server;
+
+use \SplFileObject;
+
+class Start extends Command
+{
+    protected function configure()
+    {
+        $this->setName('server:start')
+             ->setDescription('Start supervisord');
+    }
+
+    protected function execute( InputInterface $input, OutputInterface $output )
+    {        
+        $config = new Config;
+        $config->import( new SplFileObject( 'default.conf' ) );
+
+        $server = new Server( $config );
+        $server->restart();
+        
+        $output->writeln( '<info>[starting supervisord]</info>' );
+    }
+}
