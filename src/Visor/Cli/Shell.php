41bb27998a1e755c8fdb593182222fc87e36c029
Peter Johnson <insomnia@rcpt.at>
2012-07-10 18:16:00 +0100

    Removed var_dump

--- src/Visor/Cli/Shell.php
+++ src/Visor/Cli/Shell.php
@@ -9,8 +9,6 @@ class Shell
         $exitCode = 0;
         $output = exec( $command . ' 2>&1', $lines, $exitCode );
         
-        var_dump( $output, $command, $lines, $exitCode );
-        
         if( $exitCode > 0 )
         {
             throw new CliException( $output, $exitCode );
